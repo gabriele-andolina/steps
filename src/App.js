cba4888dc045e7ec10730d654970c8b7f7770522
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-02 19:13:50 +0200

    Created app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
 export default function App() {
-    return <div>Hello React!</div>;
+    return (
+        <div className="steps">
+            <div className="numbers">
+                <div className="active">1</div>
+                <div>2</div>
+                <div>3</div>
+            </div>
+
+            <p className="message">Hello</p>
+
+            <div className="buttons">
+                <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+                    Previous
+                </button>
+                <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
 }
