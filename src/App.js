1716e4e497d81d529430b7434e4380254e21c145
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-07 23:44:11 +0200

    Updated setStep and setIsOpen with callback func to update state based on current value

--- src/App.js
+++ src/App.js
@@ -11,15 +11,18 @@ export default function App() {
     const [isOpen, setIsOpen] = useState(true);
 
     function handlePrevious() {
-        if (step > 1) setStep(step - 1);
+        if (step > 1) setStep((s) => s - 1);
     }
 
     function handleNext() {
-        if (step < 3) setStep(step + 1);
+        if (step < 3) setStep((s) => s + 1);
     }
 
     return (
-        <div>
+        <>
+            <button className="close" onClick={() => setIsOpen((is) => !is)}>
+                &times;
+            </button>
             {isOpen && (
                 <div className="steps">
                     <div className="numbers">
@@ -54,6 +57,6 @@ export default function App() {
                     </div>
                 </div>
             )}
-        </div>
+        </>
     );
 }
