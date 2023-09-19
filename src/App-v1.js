3a643931bd6f00b61d67f6262a3930fbb5045649
gabriele-andolina <gabe.andolina@gmail.com>
2023-09-19 21:57:49 +0200

    Components: Created Button and refactored button code in App

--- src/App-v1.js
+++ src/App-v1.js
@@ -36,35 +36,27 @@ export default function App() {
                     </p>
 
                     <div className="buttons">
-                        <Button
-                            bgColor="#7950f2"
-                            textColor="#fff"
+                        <button
+                            style={{
+                                backgroundColor: "#7950f2",
+                                color: "#fff",
+                            }}
                             onClick={handlePrevious}
-                            text="Previous"
-                        />
-                        <Button
-                            bgColor="#7950f2"
-                            textColor="#fff"
+                        >
+                            Previous
+                        </button>
+                        <button
+                            style={{
+                                backgroundColor: "#7950f2",
+                                color: "#fff",
+                            }}
                             onClick={handleNext}
-                            text="Next"
-                        />
+                        >
+                            Next
+                        </button>
                     </div>
                 </div>
             )}
         </>
-    );
-}
-
-function Button({ textColor, bgColor, onClick, text }) {
-    return (
-        <button
-            style={{
-                backgroundColor: bgColor,
-                color: textColor,
-            }}
-            onClick={onClick}
-        >
-            {text}
-        </button>
     );
 }
